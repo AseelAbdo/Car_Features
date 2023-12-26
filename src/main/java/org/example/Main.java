68bbdc300a79b73e979b7a0a8798487fe64c745e
AseelAbdo <148259408+AseelAbdo@users.noreply.github.com>
2023-12-26 17:24:50 +0200

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,10 +3,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-
-
-
-
+    throw new UnsupportedOperationException("This method is not implemented.");
 
     }
 }
