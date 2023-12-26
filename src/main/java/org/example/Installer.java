60419d0a2b806a3eb89803109c3f86db7068f27b
AseelAbdo <148259408+AseelAbdo@users.noreply.github.com>
2023-12-26 17:49:34 +0200

    Update Installer.java

--- src/main/java/org/example/Installer.java
+++ src/main/java/org/example/Installer.java
@@ -8,7 +8,7 @@ import java.util.Properties;
 
 public class Installer {
     private String email;
-    private static final Logger logger = Logger.getLogger(Installer.class.getName());
+    private static final Logger logger1 = Logger.getLogger(Installer.class.getName());
 
     public void setEmail(String email) {
         this.email = email;
@@ -18,41 +18,11 @@ public class Installer {
         return email;
     }
 
-    public Installer(String email) {
-        this.email = email;
+    public Installer(String emailtoSend) {
+        this.email = emailtoSend;
     }
 
-    public void sendMessage( String email) {
-
-        String from = "abdoaseel2002@gmail.com";
-        String to = email;
-        try {
-            Properties properties = System.getProperties();
-            properties.put("mail.smtp.host", "smtp.gmail.com");
-            properties.put("mail.smtp.port", "587");
-            properties.put("mail.smtp.auth", "true");
-            properties.put("mail.smtp.starttls.enable", "true");
-
-            Session session = Session.getDefaultInstance(properties, new javax.mail.Authenticator() {
-                @Override
-                protected PasswordAuthentication getPasswordAuthentication() {
-                    return new PasswordAuthentication("abdoaseel2002@gmail.com", "zmtjtexsmsfqllnu");
-                }
-            });
-            session.setDebug(true);
-            MimeMessage message = new MimeMessage(session);
-            message.setFrom(new InternetAddress(from));
-            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to, false));
-            message.setSubject("Installation Request");
-            // Now set the actual message
-            message.setText("A new installation request is received.");
-
-            // Send message
-            Transport.send(message);
-            logger.info("Sent message successfully....");
-        } catch (MessagingException m) {
-        }
-    }
+ 
 
     public String receiveInstallationRequest(String requestDetails) {
         return  requestDetails;
