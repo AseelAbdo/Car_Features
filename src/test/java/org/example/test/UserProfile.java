44aa4fddb8cef08ad697a80053ede8e25fd78b8f
AseelAbdo <148259408+AseelAbdo@users.noreply.github.com>
2023-12-26 15:23:45 +0200

    Update UserProfile.java

--- src/test/java/org/example/test/UserProfile.java
+++ src/test/java/org/example/test/UserProfile.java
@@ -49,7 +49,7 @@ Customer customer = new Customer(1,"aseel","aseel@gmail.com","12345");
 
      string = customer.getEmail();
      string2="aseel2002@gmail.com";
-     customer.UpdateEmail(string2);
+     customer.updateEmail(string2);
      if(customer.getEmail().equals(string)){
     System.out.println("Email doesnt updated ");
 
