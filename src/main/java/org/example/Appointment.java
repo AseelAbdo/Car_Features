45224d73fb3cda8c2923486a92b1ee8b5409ee9b
AseelAbdo <148259408+AseelAbdo@users.noreply.github.com>
2023-12-26 14:42:25 +0200

    Update Appointment.java

--- src/main/java/org/example/Appointment.java
+++ src/main/java/org/example/Appointment.java
@@ -4,10 +4,6 @@ import java.time.LocalDateTime;
 
 public class Appointment {
     private int appointmentId;
-    private Customer customer;
-    private Installer installer;
-    private Product product;
-    private LocalDateTime scheduledTime;
     private boolean isCanceled;
 
     public Integer getID(){
@@ -23,10 +19,6 @@ public void setIsCanceled(boolean isCanceled){
 
     public Appointment(int appointmentId, Installer installer, Product product, LocalDateTime scheduledTime) {
         this.appointmentId = appointmentId;
-//        this.customer = customer;
-        this.installer = installer;
-        this.product = product;
-        this.scheduledTime = scheduledTime;
         this.isCanceled = false;
     }
 
